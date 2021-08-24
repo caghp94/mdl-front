6573c38d1d61534b4fdf814f7af72123d6e8e55c
Andrea Alexandra Monroy Carrillo <andrea.monroy@globant.com>
2021-08-23 22:39:41 -0500

    test service

--- src/app/admin/services/clients.service.ts
+++ src/app/admin/services/clients.service.ts
@@ -12,9 +12,10 @@ export class ClientsService {
 
     getClients(data: any): Observable<any>{
         // data.numsolicitud = Math.random();
-        data.numsolicitud = '12345';
-        delete data.documentIdName;
-        data.documentIdNumber.length > 8 ? data.documentIdType = "1" : data.documentIdType = "2";
-        return this.http.post(`${this.baseUrl}/api/rtg`, data)
+        // data.numsolicitud = '12345';
+        // delete data.documentIdName;
+        // data.documentIdNumber.length > 8 ? data.documentIdType = "1" : data.documentIdType = "2";
+        return data.codeId !== '' ? this.http.get(`${this.baseUrl}/api/cliente/filtrar?codigounico=${data.codeId}`) : data.numerodocumento !== '' ? this.http.get(`${this.baseUrl}/api/cliente/filtrar?numerodocumento=${data.documentIdNumber}`) : this.http.get(`${this.baseUrl}/api/cliente/filtrar?razonsocial=${data.documentIdName}`)
+        // return this.http.post(`${this.baseUrl}/api/rtg`, data)
     }
-} 
+}
